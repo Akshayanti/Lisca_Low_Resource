7e0f19dfe2d1d1ef3c044333d7a45f8146fc47af
aaggarwal <aaggarwal@twilio.com>
2020-10-05 20:57:51 +0200

    Completed for hi

--- languageSpecificScripts/hi/main.py
+++ languageSpecificScripts/hi/main.py
@@ -1,22 +1,28 @@
 import sys
+from tqdm import tqdm
 
 
 def process_input(input_file):
     output_list = []
     with open(input_file, 'r', encoding='utf-8') as infile:
         for lines in infile:
+            if lines.strip("\n").lstrip().rstrip() == "":
+                continue
             line = lines.strip('\n')
             if '।' not in line:
                 continue
             for x in line.split('।'):
                 if x.lstrip().rstrip() != '':
                     output_list.append(x.lstrip().rstrip() + '।')
-    return " ".join(output_list)
+    text = " ".join(output_list)
+    return text if text != "" else None
 
 
 def process_input_files(files_list):
     new_list = sorted(files_list, key=lambda x:int(x.split("/")[-1].split(".txt")[0]))
     a = ""
-    for x in new_list:
-        a += process_input(x) + "\n"
+    for x in tqdm(new_list, desc="Files Completed", ncols=100):
+        text = process_input(x)
+        if text:
+            a += text + "\n"
     return a
--- makefile
+++ makefile
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-get_data:
+.PHONY: preprocess
+
+preprocess:
+	pip3 install -r requirements.txt;
+	if [ ! -d processedData ]; then \
+		mkdir processedData; \
+	fi;
+
+get_data: preprocess
 	sh generalScripts/get_pud.sh;
-	sh languageSpecificScripts/hi/extractData.sh;
+	sh languageSpecificScripts/hi/prepareData.sh;
